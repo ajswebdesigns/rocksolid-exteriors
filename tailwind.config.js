c92ca1ba98e4fd72797f0dce0e7b9c1b612fc87c
Andrew Nichols <andrew@ajswebdesigns.com>
2024-09-15 14:03:37 -0400

    More homepage progess, logo section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         secondary: '#ed0000',
         accent: '#8D0003',
         gray: "#F2F2F2",
+        graymedium: "#E7E7E7",
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
