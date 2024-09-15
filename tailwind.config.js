8c9a9072fb3f5a2444f9227480d63b487833f02c
Andrew Nichols <andrew@ajswebdesigns.com>
2024-09-15 15:30:42 -0400

    added icions

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         accent: '#8D0003',
         gray: "#F2F2F2",
         graymedium: "#E7E7E7",
+        graylight: "#EFEFEF"
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
